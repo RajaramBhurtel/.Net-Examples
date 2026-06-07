367cf8082d7a38574b2d445150a748a36d164158
RajaramBhurtel <sabinbhurtel@gmail.com>
2026-06-07 22:07:05 +0545

    asp new

--- Chapter 4/ASP/StudentForm.aspx
+++ Chapter 4/ASP/StudentForm.aspx
@@ -1,9 +1,10 @@
-<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentForm.aspx.cs" Inherits="WebApp.StudentForm" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentForm.aspx.cs" Inherits="WebApplication1.StudentForm" %>
 
     <!DOCTYPE html>
+
     <html>
 
-    <head>
+    <head runat="server">
         <title>Student Registration</title>
     </head>
 
@@ -11,25 +12,85 @@
 
         <form id="form1" runat="server">
 
-            <h2>Insert Student Data</h2>
+            <h2>Student Registration Form</h2>
 
-            Name:
+            <asp:Image ID="imgCollege" runat="server" Width="150" Height="150" ImageUrl="images/college.png" />
+
+            <br /><br />
+
+            Full Name:
+
             <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+
             <br /><br />
 
             Email:
+
             <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+
             <br /><br />
 
-            <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+            Gender:
+
+            <asp:RadioButton ID="rbMale" runat="server" GroupName="Gender" Text="Male" />
+
+            <asp:RadioButton ID="rbFemale" runat="server" GroupName="Gender" Text="Female" />
+
+            <br /><br />
+
+            Department:
+
+            <asp:DropDownList ID="ddlDepartment" runat="server">
+
+                <asp:ListItem>Computer Science</asp:ListItem>
+                <asp:ListItem>Information Technology</asp:ListItem>
+                <asp:ListItem>Business Studies</asp:ListItem>
+                <asp:ListItem>Engineering</asp:ListItem>
+
+            </asp:DropDownList>
+
+            <br /><br />
+
+            Hobbies:
+
+            <br />
+
+            <asp:CheckBox ID="chkReading" runat="server" Text="Reading" />
+
+            <asp:CheckBox ID="chkSports" runat="server" Text="Sports" />
+
+            <asp:CheckBox ID="chkMusic" runat="server" Text="Music" />
+
+            <br /><br />
+
+            Country:
+
+            <asp:ListBox ID="lstCountry" runat="server">
+
+                <asp:ListItem>Nepal</asp:ListItem>
+                <asp:ListItem>India</asp:ListItem>
+                <asp:ListItem>China</asp:ListItem>
+                <asp:ListItem>USA</asp:ListItem>
+
+            </asp:ListBox>
+
+            <br /><br />
+
+            <asp:Button ID="btnSave" runat="server" Text="Save Student" OnClick="btnSave_Click" />
 
             <br /><br />
 
             <asp:Label ID="lblMessage" runat="server" ForeColor="Green"></asp:Label>
 
+            <br /><br />
+
+            <asp:HyperLink ID="hlGoogle" runat="server" NavigateUrl="https://www.google.com" Text="Visit Google"
+                Target="_blank">
+            </asp:HyperLink>
+
             <hr />
 
-            <h2>Student List</h2>
+            <h3>Inserted Students</h3>
 
             <asp:GridView ID="GridView1" runat="server">
             </asp:GridView>
--- Chapter 4/ASP/StudentForm.aspx.cs
+++ Chapter 4/ASP/StudentForm.aspx.cs
@@ -1,58 +1,112 @@
 using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Configuration;
+using MySql.Data.MySqlClient;
 
-public partial class StudentForm : System.Web.UI.Page
+namespace WebApplication1
 {
-    string conStr =
-        ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
-
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class StudentForm : System.Web.UI.Page
     {
-        if (!IsPostBack)
+        string connStr =
+            ConfigurationManager
+            .ConnectionStrings["MyDB"]
+            .ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadStudents();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string gender = "";
+
+            if (rbMale.Checked)
+                gender = "Male";
+
+            if (rbFemale.Checked)
+                gender = "Female";
+
+            string hobbies = "";
+
+            if (chkReading.Checked)
+                hobbies += "Reading,";
+
+            if (chkSports.Checked)
+                hobbies += "Sports,";
+
+            if (chkMusic.Checked)
+                hobbies += "Music,";
+
+            string country = lstCountry.SelectedItem.Text;
+
+            using (MySqlConnection con =
+                new MySqlConnection(connStr))
+            {
+                string query =
+                @"INSERT INTO Students
+                (FullName,Email,Gender,
+                 Department,Hobbies,Country)
+
+                VALUES
+                (@name,@email,@gender,
+                 @department,@hobbies,@country)";
+
+                MySqlCommand cmd =
+                    new MySqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue(
+                    "@name", txtName.Text);
+
+                cmd.Parameters.AddWithValue(
+                    "@email", txtEmail.Text);
+
+                cmd.Parameters.AddWithValue(
+                    "@gender", gender);
+
+                cmd.Parameters.AddWithValue(
+                    "@department",
+                    ddlDepartment.SelectedValue);
+
+                cmd.Parameters.AddWithValue(
+                    "@hobbies", hobbies);
+
+                cmd.Parameters.AddWithValue(
+                    "@country", country);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                lblMessage.Text =
+                    "Student Saved Successfully";
+            }
+
             LoadStudents();
         }
-    }
 
-    protected void btnSave_Click(object sender, EventArgs e)
-    {
-        SqlConnection con = new SqlConnection(conStr);
+        private void LoadStudents()
+        {
+            using (MySqlConnection con =
+                new MySqlConnection(connStr))
+            {
+                string query =
+                    "SELECT * FROM Students";
 
-        string query =
-            "INSERT INTO Students(Name, Email) VALUES(@Name, @Email)";
+                MySqlDataAdapter da =
+                    new MySqlDataAdapter(query, con);
 
-        SqlCommand cmd = new SqlCommand(query, con);
+                DataTable dt =
+                    new DataTable();
 
-        cmd.Parameters.AddWithValue("@Name", txtName.Text);
-        cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                da.Fill(dt);
 
-        con.Open();
-        cmd.ExecuteNonQuery();
-        con.Close();
-
-        lblMessage.Text = "Data Inserted Successfully";
-
-        txtName.Text = "";
-        txtEmail.Text = "";
-
-        LoadStudents();
-    }
-
-    private void LoadStudents()
-    {
-        SqlConnection con = new SqlConnection(conStr);
-
-        string query = "SELECT * FROM Students";
-
-        SqlDataAdapter da = new SqlDataAdapter(query, con);
-
-        DataTable dt = new DataTable();
-
-        da.Fill(dt);
-
-        GridView1.DataSource = dt;
-        GridView1.DataBind();
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
     }
 }
